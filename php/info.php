163c47f8a5740b1a8922abdee6131cd7fe94c2d8
Nannu9057 <89435711+Nannu9057@users.noreply.github.com>
2025-02-01 04:55:26 +0530

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,5 +1,5 @@
 <?php 
-$base_url = "//{$_SERVER['SERVER_NAME']}";
+$base_url = "//{$_SERVER['animelax.xyz']}";
 $website_name = "AnimeLax";
 $apiLink = ""; // add the api here with the '/' at the end!
 $consumet = ""; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
